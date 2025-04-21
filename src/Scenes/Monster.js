3a969785e0993768b5c05fc1a84b9cc881bf715c
Bryce <Bhan16@ucsc.edu>
2025-04-20 18:45:22 -0700

    Finished monster

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -27,7 +27,7 @@ class Monster extends Phaser.Scene {
 
         // Create the main body sprite
         //
-        // this.add.sprite(x,y, "{atlas key name}", "{name of sprite within atlas}")
+        // this.add.sprite(x,y, "{atlas D name}", "{name of sprite within atlas}")
         //
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
@@ -49,30 +49,79 @@ class Monster extends Phaser.Scene {
         my.sprite.leftArm.flipX=true;
 
         // Mouth
-        my.sprite.mouth = this.add.sprite(this.bodyX, this.bodyY,"monsterParts", "eye_yellow.png" );
+        my.sprite.mouth = this.add.sprite(this.bodyX, this.bodyY,"monsterParts", "mouthA.png" );
+        my.sprite.fangs = this.add.sprite(this.bodyX, this.bodyY+20,"monsterParts","mouth_closed_fangs.png");
+        my.sprite.fangs.visible = false;
 
         // right eye
         my.sprite.rightEye = this.add.sprite(this.bodyX+20, this.bodyY-20,"monsterParts", "eye_yellow.png" );
         my.sprite.rightEye.setScale(0.65);
 
         // left eye
-        my.sprite.leftEye = this.add.sprite(this.bodyX-20, this.bodyY-20,"monsterParts", "mouthG.png" );
+        my.sprite.leftEye = this.add.sprite(this.bodyX-20, this.bodyY-20,"monsterParts", "eye_yellow.png" );
         my.sprite.leftEye.setScale(0.65);
+
+        // Horns
+        my.sprite.rightHorn = this.add.sprite(this.bodyX+40, this.bodyY-75,"monsterParts", "detail_red_horn_large.png" );
+        my.sprite.leftHorn = this.add.sprite(this.bodyX-40, this.bodyY-75,"monsterParts", "detail_red_horn_large.png" );
+        my.sprite.leftHorn.flipX = true;
 
 
         // Use container to group body parts
-        myContainer = this.add.container(100, 100);
+
+        this.my.container = this.add.container(100, 100);
 
         // Add all body parts to container
-        myContainer.add(my.sprite.body);
-        myContainer.add(my.sprite.rightArm);
-        myContainer.add(my.sprite.leftArm);
+        this.my.container.add(my.sprite.body);
+        this.my.container.add(my.sprite.rightArm);
+        this.my.container.add(my.sprite.leftArm);
+        this.my.container.add(my.sprite.rightLeg);
+        this.my.container.add(my.sprite.leftLeg);
+        this.my.container.add(my.sprite.mouth);
+        this.my.container.add(my.sprite.fangs);
+        this.my.container.add(my.sprite.rightEye);
+        this.my.container.add(my.sprite.leftEye);
+        this.my.container.add(my.sprite.rightHorn);
+        this.my.container.add(my.sprite.leftHorn);
+
+        
+        this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
 
     }
 
     update() {
         let my = this.my;    // create an alias to this.my for readability
+
+        if (this.keyS.isDown) {
+            my.sprite.fangs.visible = false;
+            my.sprite.mouth.visible = true;
+        }
+
+        if (this.keyF.isDown) {
+            my.sprite.mouth.visible = false;
+            my.sprite.fangs.visible = true;
+        }
+
+        if (this.keyA.isDown) {
+            my.container.x -= 1;
+            
+        }else if (this.keyA.isUp) {
+            my.container.x -= 0;
+        }
+
+        if (this.keyD.isDown) {
+            my.container.x += 1;
+            
+        }else if (this.keyD.isUp) {
+            my.container.x += 0;
+        }
+
+
+        
 
        
     }
